7578c151648979d7b63f54d8951895415166d966
Philippe Serhal <philippe.serhal@gmail.com>
2014-09-19 21:52:23 -0400

    Add ability to create partial decks.
    These may also serve as "stacks" of cards, by piping the output of a draw from
    a deck into a new partial deck.

--- spec/rest-deck.spec.js
+++ spec/rest-deck.spec.js
@@ -5,10 +5,10 @@ describe('rest-cards /deck/', function() {
         'url': 'http://localhost:' + (process.env.PORT || 8080)
     });
 
-    var decks = [];
+    var decks = {};
 
     describe('creation', function() {
-        it('creates a new deck by ID', function(done) {
+        it('creates a new full deck by ID', function(done) {
             client.put('/deck/testdeck', function(err, req, res, obj) {
                 expect(err).toBeNull();
                 expect(res.statusCode).toBe(201);
@@ -18,12 +18,12 @@ describe('rest-cards /deck/', function() {
                     expect(card).toEqual(jasmine.any(String));
                     expect(card.length).toEqual(8); // e.g. '/card/D3'
                 });
-                decks.push('/deck/testdeck');
+                decks['/deck/testdeck'] = 52;
                 done();
             });
         });
 
-        it('overwrites an existing deck', function(done) {
+        it('overwrites an existing deck with a full one', function(done) {
             client.put('/deck/testdeck', function(err, req, res, obj) {
                 expect(err).toBeNull();
                 expect(res.statusCode).toBe(200);
@@ -37,25 +37,71 @@ describe('rest-cards /deck/', function() {
             });
         });
 
-        it('creates and names a new deck', function(done) {
+        it('creates and names a new full deck', function(done) {
             client.post('/deck', function(err, req, res, obj) {
                 expect(err).toBeNull();
                 expect(res.statusCode).toBe(201);
                 expect(res.headers['location']).toBeDefined();
-                decks.push(res.headers['location']);
+                decks[res.headers['location']] = 52;
+                done();
+            });
+        });
+
+        it('creates a new partial deck by ID', function(done) {
+            client.put('/deck/testpartialdeck',
+                       ['/card/D3', '/card/QH', '/card/9D'],
+                       function(err, req, res, obj) {
+                expect(err).toBeNull();
+                expect(res.statusCode).toBe(201);
+                expect(obj).toEqual(jasmine.any(Array));
+                console.log(obj.length);
+                expect(obj.length).toEqual(3);
+                obj.forEach(function(card) {
+                    expect(card).toEqual(jasmine.any(String));
+                    expect(card.length).toEqual(8); // e.g. '/card/D3'
+                });
+                decks['/deck/testpartialdeck'] = 3;
+                done();
+            });
+        });
+
+        it('overwrites an existing deck with a partial one', function(done) {
+            client.put('/deck/testpartialdeck',
+                       ['/card/D3', '/card/QH'],
+                       function(err, req, res, obj) {
+                expect(err).toBeNull();
+                expect(res.statusCode).toBe(200);
+                expect(obj).toEqual(jasmine.any(Array));
+                expect(obj.length).toEqual(2);
+                obj.forEach(function(card) {
+                    expect(card).toEqual(jasmine.any(String));
+                    expect(card.length).toEqual(8); // e.g. '/card/D3'
+                });
+                done();
+            });
+        });
+
+        it('creates and names a new partial deck', function(done) {
+            client.post('/deck',
+                       ['/card/D3', '/card/QH', '/card/9D'],
+                       function(err, req, res, obj) {
+                expect(err).toBeNull();
+                expect(res.statusCode).toBe(201);
+                expect(res.headers['location']).toBeDefined();
+                decks[res.headers['location']] = 3;
                 done();
             });
         });
     });
 
     describe('retrieval', function() {
-        decks.forEach(function(path) {
+        Object.keys(decks).forEach(function(path) {
             it('gets a deck by ID (' + path + ')', function(done) {
                 client.get(path, function(err, req, res, obj) {
                     expect(err).toBeNull();
                     expect(res.statusCode).toBe(200);
                     expect(obj).toEqual(jasmine.any(Array));
-                    expect(obj.length).toEqual(52);
+                    expect(obj.length).toEqual(decks[path]);
                     obj.forEach(function(card) {
                         expect(card).toEqual(jasmine.any(String));
                         expect(card.length).toEqual(8); // e.g. '/card/D3'
@@ -68,7 +114,7 @@ describe('rest-cards /deck/', function() {
                 client.get(path + '/size', function(err, req, res, obj) {
                     expect(err).toBeNull();
                     expect(res.statusCode).toBe(200);
-                    expect(obj).toEqual(52);
+                    expect(obj).toEqual(decks[path]);
                     done();
                 });
             });
@@ -163,8 +209,17 @@ describe('rest-cards /deck/', function() {
     });
 
     describe('deletion', function() {
-        it('deletes an existing deck', function(done) {
+        it('deletes an empty deck', function(done) {
             client.del('/deck/testdeck', function(err, req, res, obj) {
+                expect(err).toBeNull();
+                expect(res.statusCode).toBe(204);
+                expect(obj).toEqual({});
+                done();
+            });
+        });
+
+        it('deletes a nonempty deck', function(done) {
+            client.del('/deck/testpartialdeck', function(err, req, res, obj) {
                 expect(err).toBeNull();
                 expect(res.statusCode).toBe(204);
                 expect(obj).toEqual({});
